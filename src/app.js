a513101eaa48ab7981e3463041b5a4ffcef257c7
lauclopez <lozanoabello18@gmail.com>
2023-08-24 19:30:45 -0400

    updated the class and id's names to make it easier to read

--- src/app.js
+++ src/app.js
@@ -4,16 +4,18 @@ function calculateBmi() {
   let heightCm = document.getElementById("height").value;
   let heightMeters = heightCm / 100;
   let bmi = weight / (heightMeters * heightMeters);
+  bmi = Math.round(bmi);
   let result = document.getElementById("result");
+  let info = `<div class="information"> To get more information about your results <a href="second.html">Click here</a> </div>`;
 
   if (bmi < 18.5) {
-    result.innerHTML = "Underweight";
+    result.innerHTML = `<BMI : ${bmi}  </br> Underweight ${info}`;
   } else if (bmi <= 24.9) {
-    result.innerHTML = "Normal weight";
+    result.innerHTML = `BMI : ${bmi} </br> Normal weight ${info}`;
   } else if (bmi <= 29.9) {
-    result.innerHTML = "Overweight";
+    result.innerHTML = `BMI : ${bmi} </br> Overweight ${info}`;
   } else {
-    result.innerHTML = "Obesity";
+    result.innerHTML = `BMI : ${bmi}</br> Obesity ${info}`;
   }
 }
 
